954e44f34a3cb266fddcda813ef7295f5abe8490
xiaoqiang001 <kang991005214@163.com>
2017-06-22 11:18:04 +0800

    base64

---
+++ imgtobase64/js.js
@@ -0,0 +1,20 @@
+function convertImgToBase64(url, callback, outputFormat){
+    var canvas = document.createElement('CANVAS'),
+        ctx = canvas.getContext('2d'),
+        img = new Image;
+    img.crossOrigin = 'Anonymous';
+    img.onload = function(){
+        canvas.height = img.height;
+        canvas.width = img.width;
+        ctx.drawImage(img,0,0);
+        var dataURL = canvas.toDataURL(outputFormat || 'image/png');
+        callback.call(this, dataURL);
+        canvas = null; 
+    };
+    img.src = url;
+}
+
+
+convertImgToBase64('http://bit.ly/18g0VNp', function(base64Img){
+    // Base64DataURL
+});
